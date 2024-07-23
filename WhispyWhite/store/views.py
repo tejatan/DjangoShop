7375923cc09b0b6c18d42ed2f35d9f6b2cdea103
tejatan <teja.tanm@gmail.com>
2024-07-23 07:45:10 +0530

    Cart Funtionality Added

--- WhispyWhite/store/views.py
+++ WhispyWhite/store/views.py
@@ -1,6 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from category.models import Category# Assuming you have a Product model
 from store.models import Product
+from carts.models import CartItem
+from carts.views import _cart_id
+
 
 def store_view(request, category_slug=None):
     catogaries = None
@@ -18,9 +22,11 @@ def store_view(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
     except Exception as e:
         raise e
     context = {
         'single_product': single_product,
+        'in_cart': in_cart,
     }
     return render(request, 'store/product_detail.html', context)
